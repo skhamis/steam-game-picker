b82e69e67b9cb427bba1ae6c87a181f6656bf259
Sammy Khamis <sbkhamis@gmail.com>
2019-02-14 09:24:08 -1000

    fix deploying issues

--- index.js
+++ index.js
@@ -67,7 +67,19 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.static(path.join(__dirname, "client/build")));
+//production mode
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  //
+  app.get("*", (req, res) => {
+    res.sendfile(path.join((__dirname = "client/build/index.html")));
+  });
+}
+
+//build mode
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+});
 
 // Test API to send whatevs
 app.get("/api/test", function(req, res) {
